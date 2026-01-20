dee6f6b258454ebf199824a0a5a3a6dcbb75777b
Colin Brown <colinmakerofthings@gmail.com>
2026-01-20 21:15:08 +0000

    Add cleanup functionality to move non-video files to Deleted folder

--- src/video_sweep/finder.py
+++ src/video_sweep/finder.py
@@ -1,13 +1,17 @@
 import os
-from typing import List
 
 VIDEO_EXTENSIONS = {'.mp4', '.mkv', '.avi'}
 
-def find_videos(source_dir: str) -> List[str]:
-    """Recursively find video files in the source directory."""
+
+def find_files(source_dir: str):
+    """Recursively find all files in the source directory, returning (videos, non_videos)."""
     videos = []
+    non_videos = []
     for root, _, files in os.walk(source_dir):
         for file in files:
+            full_path = os.path.join(root, file)
             if os.path.splitext(file)[1].lower() in VIDEO_EXTENSIONS:
-                videos.append(os.path.join(root, file))
-    return videos
+                videos.append(full_path)
+            else:
+                non_videos.append(full_path)
+    return videos, non_videos
